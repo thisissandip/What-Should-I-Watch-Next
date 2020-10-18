ee3686aba581338d63e4bc4fab9a8a4c312f22f6
thisissandip <69596988+thisissandip@users.noreply.github.com>
2020-10-19 00:53:45 +0530

    Updates

---
+++ precache-manifest.84779f4e0809253538f412f4406ca449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c393f684824ada5b3c423c7bef5247",
+    "url": "/What-Should-I-Watch-Next/index.html"
+  },
+  {
+    "revision": "bf5e6714b32fbee2e608",
+    "url": "/What-Should-I-Watch-Next/static/css/main.d10d7d28.chunk.css"
+  },
+  {
+    "revision": "7dd4c5466bc8de5abb25",
+    "url": "/What-Should-I-Watch-Next/static/js/2.1e049cf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/What-Should-I-Watch-Next/static/js/2.1e049cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5e6714b32fbee2e608",
+    "url": "/What-Should-I-Watch-Next/static/js/main.cd40e004.chunk.js"
+  },
+  {
+    "revision": "9461917fb03e067ef05f",
+    "url": "/What-Should-I-Watch-Next/static/js/runtime-main.56a869ad.js"
+  },
+  {
+    "revision": "d54d3ecfbfb354f3b545deaf467c7c06",
+    "url": "/What-Should-I-Watch-Next/static/media/LOGO1.d54d3ecf.png"
+  }
+]);
